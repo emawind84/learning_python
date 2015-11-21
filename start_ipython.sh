99f234b127b88943e7a4a08bfe92e6354e347cf6
Emanuele Disco <emawind84@gmail.com>
2015-11-21 23:20:22 +0900

    ciao mammma!

--- start_ipython.sh
+++ start_ipython.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+SCRIPT_BASE_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+SCRIPT_NAME="${0##*/}"
+
+echo "Script name: [$SCRIPT_NAME]"
+echo "Base path: [$SCRIPT_BASE_PATH]"
+echo "Starting iPython notebook..."
+
+cd $SCRIPT_BASE_PATH
 source ipython/bin/activate
 
-ipython notebook
+ipython notebook >/dev/null 2>&1 &
