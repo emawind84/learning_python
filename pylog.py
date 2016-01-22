60d3a0756bf6fcfce4cf824eb51cd59d1732e456
Emanuele Disco <emawind84@gmail.com>
2016-01-23 02:26:46 +0900

    updates

--- pylog.py
+++ pylog.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 from __future__ import print_function, division
 from datetime import datetime
-import atexit, os.path
+import atexit, os.path, sys
 
+ori_stdout = sys.__stdout__
 FILE_NAME = 'log.log'
 WRITE_FREQ = 10
 
@@ -12,7 +13,7 @@ def _get_filename():
     return FILE_NAME
 
 def set_header(header):
-    print('Setting header...')
+    #print('Setting header...', file=ori_stdout)
     if os.path.isfile(_get_filename()):
         raise Exception('Logging file already exists!')
         
@@ -22,8 +23,9 @@ def set_header(header):
 def write_on_file():
     global batch_data
     with open(_get_filename(), 'a') as f:
-        print("Writing log to file...")
+        #print("Writing log to file...", file=ori_stdout)
         for line in batch_data:
+            #print('line: %s' % line, file=ori_stdout)
             f.write(line + '\n')
         batch_data = []
         
@@ -34,12 +36,21 @@ def log_data(data):
         write_on_file()
 
 def log(msg):
-    dt = datetime.utcnow()
+    dt = datetime.now()
     datestr = dt.strftime('%Y-%m-%d %H:%M:%S')
     batch_data.append('[%s] %s' % (datestr, msg))
     if len(batch_data) >= WRITE_FREQ:
         write_on_file()
-    
+
+class PyLog:
+    # for manual stream redirection
+    # sys.stdout = pylog
+    def write(self, msg):
+        log(msg)
+        
+    def flush(self):
+        pass
+
 def main():
     log('This is a test message! Ciao!')
     
