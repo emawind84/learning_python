7fe9cbf04e92398cb6183b1f707e644c405f8dca
Emanuele Disco <emawind84@gmail.com>
2016-01-29 00:23:55 +0900

    update

--- maker_request.py
+++ maker_request.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python
 import requests, argparse, json
 
-maker_key = 'nmr2BnBoPJPDkNvfz3bk0'
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-e', action='store', dest='event', type=str, help='Event to trigger')
-parser.add_argument('-p', action='store', dest='params', type=str, help='Parameters to send with the event')
-args = parser.parse_args()
+_maker_key = 'nmr2BnBoPJPDkNvfz3bk0'
+_args = {}
 
 def send(event, params=None):
     # https://maker.ifttt.com/trigger/{event}/with/key/nmr2BnBoPJPDkNvfz3bk0
-    r = requests.get("https://maker.ifttt.com/trigger/%s/with/key/%s" % (event, maker_key), data=params)
+    r = requests.get("https://maker.ifttt.com/trigger/%s/with/key/%s" % (event, _maker_key), data=params)
     r.text
     
 def main():
-    if not args.event:
+    if not _args.event:
         print('Event not defined')
         return
-    if args.params:
+    if _args.params:
         try:
             p = json.loads(args.params)
         except:
             print('Parameters not well format. JSON format required.')
             return
-    send(args.event, p)
+    send(_args.event, p)
     
 if __name__=='__main__':
+    _parser = argparse.ArgumentParser()
+    _parser.add_argument('-e', action='store', dest='event', type=str, help='Event to trigger')
+    _parser.add_argument('-p', action='store', dest='params', type=str, help='Parameters to send with the event')
+    _args = _parser.parse_args()
+
     main()
